0f444e83e3219cfc61540d796b67f8ec8fe7faf1
Stefan <i@thechoephix.com>
2019-02-28 06:39:27 +0000

    non-ascii character fix

--- therasp/monitor.py
+++ therasp/monitor.py
@@ -4,13 +4,13 @@ from time import sleep
 cpu = CPUTemperature()
 
 while True:
-  temp = cpu.temperature
-  endc = '\033[0m'
-  clr = '\033[94m'
-  if temp > 45.0: clr = '\033[96m'
-  if temp > 50.0: clr = '\033[92m'
-  if temp > 55.0: clr = '\033[93m'
-  if temp > 60.0: clr = '\033[91m'
-  if temp > 70.0: clr = '\033[101m'
-  print("• temperature: "+clr+(str)(temp)+" degrees"+endc)
-  sleep(5)
+    temp = cpu.temperature
+    endc = '\033[0m'
+    clr = '\033[94m'
+    if temp > 45.0: clr = '\033[96m'
+    if temp > 50.0: clr = '\033[92m'
+    if temp > 55.0: clr = '\033[93m'
+    if temp > 60.0: clr = '\033[91m'
+    if temp > 70.0: clr = '\033[101m'
+    print("~ temperature: "+clr+(str)(temp)+" degrees"+endc)
+    sleep(5)
