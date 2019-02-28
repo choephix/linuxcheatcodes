cb141c1f8c567e9359abf1bd6aa2997b8d9fe297
Stefan <i@thechoephix.com>
2019-02-28 06:50:22 +0000

    test

--- therasp/monitor.py
+++ therasp/monitor.py
@@ -1,10 +1,11 @@
+import sys
 from gpiozero import CPUTemperature
-from time import sleep
+import time
 
 cpu = CPUTemperature()
 
-while True:
-    temp = cpu.temperature
+
+def print_line(temp):
     endc = '\033[0m'
     clr = '\033[94m'
     if temp > 45.0: clr = '\033[96m'
@@ -12,5 +13,15 @@ while True:
     if temp > 55.0: clr = '\033[93m'
     if temp > 60.0: clr = '\033[91m'
     if temp > 70.0: clr = '\033[101m'
-    print("~ temperature: "+clr+(str)(temp)+" degrees"+endc)
-    sleep(5)
+    clock = time.clock()
+    sys.stdout.write((str)(clock)+" ~ temperature: "+clr+(str)(temp)+" degrees"+endc)
+    sys.stdout.flush()
+
+
+interval = 1
+t_prev = 0
+while True:
+    t = time.clock()
+    if t - t_prev >= interval:
+      sys.stdout.write('\r')
+    print_line( t, cpu.temperature )
