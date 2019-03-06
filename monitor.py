9ba94b3d7ecd4240e364c4df58e62fc28d79f661
theorange <you@orange.pi>
2019-03-06 00:20:49 +0000

    ffff

--- monitor.py
+++ monitor.py
@@ -6,10 +6,11 @@ import time
 INTERVAL = 5.0
 
 
-def c(n): 
+def c(n):
   return "\033["+str(n)+"m"
 
-def print_line( t, temp ):
+
+def print_line(t, temp):
   temp_str = '{0:.3f}'.format(temp)
   clr = colors[0]
   for i, thre in enumerate(thresh):
@@ -19,30 +20,30 @@ def print_line( t, temp ):
       break
   s = ''
   s += c(30+clr)
-  s += "-> cpu temperature: "
+  s += "[ cpu temperature: "
   s += c(90+clr)+temp_str
   s += c(30+clr)
-  s += " degrees celsius"
-  s += c(35)
-  s += ' '
+  s += " degrees celsius ]"
+  # s += c(35)
+  # s += ' '
   sys.stdout.write('\r')
   sys.stdout.write(s)
   sys.stdout.flush()
 
 
 fpath = "/sys/class/thermal/thermal_zone0/temp"
-colors = [  4,  6,  2,  3,  1, 11 ]
-thresh = [ 30, 40, 50, 60, 70 ]
+colors = [4,  6,  2,  3,  1, 11]
+thresh = [30, 40, 50, 60, 70]
 
 os.system('clear')
 
 with open("/sys/firmware/devicetree/base/model") as f:
   device_name = f.read()
 print()
-print( c(35) + device_name )
+print(c(35) + device_name)
 print()
 
-# # # 
+# # #
 
 temp_max = 0.0
 t_prev = 0.0
@@ -50,7 +51,7 @@ t_prev = 0.0
 try:
   while True:
     t = time.time()
-    with open( fpath ) as f:
+    with open(fpath) as f:
       temp = .001 * float(f.read())
     if temp_max < temp:
         temp_max = temp
